d854e15d71cba06b329753c93353ff4778df8cb7
TheDarkThief <thedarkthief@mailfence.com>
2025-08-21 00:04:34 -0700

    No longer using EnchantingIngredientMapPayload so commented it out

--- src/main/java/cutefox/betterenchanting/Util/EnchantingIngredientMapPayload.java
+++ src/main/java/cutefox/betterenchanting/Util/EnchantingIngredientMapPayload.java
@@ -1,24 +1,24 @@
-// TODO figure out networking
+// No longer used
 
-package cutefox.betterenchanting.Util;
+// package cutefox.betterenchanting.Util;
 
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.CustomPayload;
+// import net.minecraft.network.RegistryByteBuf;
+// import net.minecraft.network.PacketByteBuf;
+// import net.minecraft.network.packet.CustomPayload;
 
-import java.util.*;
+// import java.util.*;
 
-import cutefox.betterenchanting.datagen.ModEnchantIngredientMap;
+// import cutefox.betterenchanting.datagen.ModEnchantIngredientMap;
 
-public record EnchantingIngredientMapPayload(Map<String, List<String>> map) implements CustomPayload {
-    public static final Id<EnchantingIngredientMapPayload> ID = new Id<>(
-            BetterEnchantingConstants.ENCHANT_INGREDIENT_MAP_PACKET_ID);
-    public static final PacketCodec<RegistryByteBuf, EnchantingIngredientMapPayload> CODEC = PacketCodec.tuple(
-            ModEnchantIngredientMap.MAP_CODEC, EnchantingIngredientMapPayload::map,
-            EnchantingIngredientMapPayload::new);
+// public record EnchantingIngredientMapPayload(Map<String, List<String>> map) implements CustomPayload {
+//     public static final Id<EnchantingIngredientMapPayload> ID = new Id<>(
+//             BetterEnchantingConstants.ENCHANT_INGREDIENT_MAP_PACKET_ID);
+//     public static final PacketCodec<RegistryByteBuf, EnchantingIngredientMapPayload> CODEC = PacketCodec.tuple(
+//             ModEnchantIngredientMap.MAP_CODEC, EnchantingIngredientMapPayload::map,
+//             EnchantingIngredientMapPayload::new);
 
-    @Override
-    public Id<? extends CustomPayload> getId() {
-        return ID;
-    }
-}
+//     @Override
+//     public Id<? extends CustomPayload> getId() {
+//         return ID;
+//     }
+// }
